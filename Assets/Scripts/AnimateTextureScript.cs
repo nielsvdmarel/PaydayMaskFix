f1f36dc45c4e13fffb0a9b2e690bead655fed0f9
nielsvdmarel <nielsvdmarel@gmail.com>
2018-01-31 01:07:10 +0100

    new textures, first little test script running

---
+++ Assets/Scripts/AnimateTextureScript.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimateTextureScript : MonoBehaviour {
+    [SerializeField]
+    private Texture[] Textures;
+   
+
+    [SerializeField]
+    private int CurrentTex;
+
+    [SerializeField]
+    private Material LedLightShader;
+    [SerializeField]
+    private Renderer rend;
+
+	void Start ()
+    {
+        rend = GetComponent<Renderer>();
+        CurrentTex = 0;
+        StartCoroutine(Wait1sec());
+	}
+
+    IEnumerator Wait1sec()
+    {
+        while (true)
+        {
+            yield return new WaitForSeconds(.5f);
+            if(CurrentTex == 1)
+            {
+                LedLightShader.SetTexture("_MainTex", Textures[0]);
+                CurrentTex = 0;
+            }
+            else
+            {
+                LedLightShader.SetTexture("_MainTex", Textures[1]);
+                CurrentTex = 1;
+            }
+            Debug.Log("Niels is cool");
+            
+        }
+    }
+}
